a6e1814046688aeba4d564dfadd174a4c755b84c
Gartox <ewan71230@gmail.com>
2023-07-24 12:01:42 +0200

    fix g++ runtime dep

--- experiments/luuna/cpp/tf_idf/gcc.sh
+++ experiments/luuna/cpp/tf_idf/gcc.sh
@@ -5,7 +5,8 @@ export EXEC_TITLE="Éxectution de tf_idf C++ (gcc)"
 
 export CLEAN="make clean"
 export BUILD="make GCC=/data/ewan/gcc/out/bin/g++"
-export EXEC="LD_LIBRARY_PATH=/data/ewan/gcc/out/lib64:$LD_LIBRARY_PATH bin/tf_idf 'war' /data/ewan/corpus/gutenberg/*"
+export LD_LIBRARY_PATH=/data/ewan/gcc/out/lib64:$LD_LIBRARY_PATH 
+export EXEC="bin/tf_idf 'war' /data/ewan/corpus/gutenberg/*"
 
 export BUILD_SAMPLE_PERIOD=100
 export EXEC_SAMPLE_PERIOD=100
