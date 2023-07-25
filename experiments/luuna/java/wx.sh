f4c6c5617592e4b919424803bab46dbc6458e2d5
Gartox <ewan71230@gmail.com>
2023-07-25 11:52:48 +0200

    Fix

--- experiments/luuna/java/wx.sh
+++ experiments/luuna/java/wx.sh
@@ -6,7 +6,7 @@ export EXEC_TITLE="Éxectution de wx Java"
 export CLEAN=""
 JAVA_HOME=/data/ewan/java/build_stage2/images/jdk
 export BUILD="$JAVA_HOME/bin/javac Wx.java"
-export EXEC="$JAVA_HOME/bin/java Wx 2048 1024"
+export EXEC="$JAVA_HOME/bin/java Wx 1024"
 
 export BUILD_SAMPLE_PERIOD=100
 export EXEC_SAMPLE_PERIOD=100
