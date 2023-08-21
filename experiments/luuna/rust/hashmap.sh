a6161c78f87cd9f7f9f50868eb8be2ed57161e74
Gartox <ewan71230@gmail.com>
2023-08-21 15:19:02 +0200

    Fix rust

--- experiments/luuna/rust/hashmap.sh
+++ experiments/luuna/rust/hashmap.sh
@@ -5,7 +5,7 @@ export EXEC_TITLE="Éxectution de hashmap Rust"
 
 export CLEAN="cargo clean"
 export BUILD="cargo +stage2 build --release -j 1"
-export EXEC="target/release/hashmap 2048 1024"
+export EXEC="target/release/hashmap 1000000"
 
 export BUILD_SAMPLE_PERIOD=100
 export EXEC_SAMPLE_PERIOD=100
